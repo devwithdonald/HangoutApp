ae35d7b1b97b37f08cb1f0afbb15b43eba160045
devwithdonald <hend.donald@gmail.com>
2019-07-17 16:56:43 -0400

    updated loginController

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -1,13 +1,24 @@
 package com.revature.controllers;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.revature.beans.User;
 import com.revature.services.UserServiceImpl;
 
 @Controller
 public class LoginController {
-
+	
+	private static Logger log = Logger.getLogger("DRIVER_LOGGER");
+	
 	UserServiceImpl usi;
 	
 	@Autowired
@@ -15,8 +26,17 @@ public class LoginController {
 		this.usi = usi;
 	}
 	
-	//@PostMapping(value="/login", consumes= {"application/json"})
-	//public @ResponseBody User
+	@PostMapping(value="/login", consumes= {"application/json"})
+	public @ResponseBody User loginPost(@RequestBody User user, HttpSession sess) {
+		log.log(Level.INFO, "Attempted Login: " + user);
+		User authUser = usi.validateUser(user);
+		if (authUser != null) {
+			sess.setAttribute("user", authUser);
+			log.log(Level.INFO, "Logged in user: " + authUser);
+			return authUser;
+		}
+		return null;
+	}
 	
 	
 }
