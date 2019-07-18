aa0a0281e33ec10ff5cb2dffd4fb6f2d7c7375e2
devwithdonald <hend.donald@gmail.com>
2019-07-18 18:58:16 -0400

    deleted friends and updated basicuser bean

--- src/main/java/com/revature/beans/BasicUser.java
+++ src/main/java/com/revature/beans/BasicUser.java
@@ -31,8 +31,6 @@ public class BasicUser extends User{
 			inverseJoinColumns=@JoinColumn(name="FRIEND_ID"))
 	private Set<BasicUser> friendList;
 	
-
-
 	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="basicUser")
 	private Set<Subscriptions> subscriptions = new HashSet<Subscriptions>();
 
@@ -47,20 +45,29 @@ public class BasicUser extends User{
 	
 	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY, mappedBy="basicUser")
 	private Set<EventMessage> eventMessages = new HashSet<EventMessage>();
-	
-	
+
 	public BasicUser() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	public BasicUser(int userId, String username, String password, Role role) {
+		super(userId, username, password, role);
+		// TODO Auto-generated constructor stub
+	}
+
 	public BasicUser(int userId, String username, String password, Role role, String firstName, String lastName,
-			Set<Friends> friendList, Set<Subscriptions> subscriptions) {
+			Set<BasicUser> friendList, Set<Subscriptions> subscriptions, Set<UserMessage> sentMessages,
+			Set<UserMessage> recievedMessages, Set<Rsvp> rsvps, Set<EventMessage> eventMessages) {
 		super(userId, username, password, role);
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.friendList = friendList;
 		this.subscriptions = subscriptions;
+		this.sentMessages = sentMessages;
+		this.recievedMessages = recievedMessages;
+		this.rsvps = rsvps;
+		this.eventMessages = eventMessages;
 	}
 
 	public String getFirstName() {
@@ -79,11 +86,11 @@ public class BasicUser extends User{
 		this.lastName = lastName;
 	}
 
-	public Set<Friends> getFriendList() {
+	public Set<BasicUser> getFriendList() {
 		return friendList;
 	}
 
-	public void setFriendList(Set<Friends> friendList) {
+	public void setFriendList(Set<BasicUser> friendList) {
 		this.friendList = friendList;
 	}
 
@@ -95,13 +102,49 @@ public class BasicUser extends User{
 		this.subscriptions = subscriptions;
 	}
 
+	public Set<UserMessage> getSentMessages() {
+		return sentMessages;
+	}
+
+	public void setSentMessages(Set<UserMessage> sentMessages) {
+		this.sentMessages = sentMessages;
+	}
+
+	public Set<UserMessage> getRecievedMessages() {
+		return recievedMessages;
+	}
+
+	public void setRecievedMessages(Set<UserMessage> recievedMessages) {
+		this.recievedMessages = recievedMessages;
+	}
+
+	public Set<Rsvp> getRsvps() {
+		return rsvps;
+	}
+
+	public void setRsvps(Set<Rsvp> rsvps) {
+		this.rsvps = rsvps;
+	}
+
+	public Set<EventMessage> getEventMessages() {
+		return eventMessages;
+	}
+
+	public void setEventMessages(Set<EventMessage> eventMessages) {
+		this.eventMessages = eventMessages;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
+		result = prime * result + ((eventMessages == null) ? 0 : eventMessages.hashCode());
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((friendList == null) ? 0 : friendList.hashCode());
 		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((recievedMessages == null) ? 0 : recievedMessages.hashCode());
+		result = prime * result + ((rsvps == null) ? 0 : rsvps.hashCode());
+		result = prime * result + ((sentMessages == null) ? 0 : sentMessages.hashCode());
 		result = prime * result + ((subscriptions == null) ? 0 : subscriptions.hashCode());
 		return result;
 	}
@@ -115,6 +158,11 @@ public class BasicUser extends User{
 		if (getClass() != obj.getClass())
 			return false;
 		BasicUser other = (BasicUser) obj;
+		if (eventMessages == null) {
+			if (other.eventMessages != null)
+				return false;
+		} else if (!eventMessages.equals(other.eventMessages))
+			return false;
 		if (firstName == null) {
 			if (other.firstName != null)
 				return false;
@@ -130,6 +178,21 @@ public class BasicUser extends User{
 				return false;
 		} else if (!lastName.equals(other.lastName))
 			return false;
+		if (recievedMessages == null) {
+			if (other.recievedMessages != null)
+				return false;
+		} else if (!recievedMessages.equals(other.recievedMessages))
+			return false;
+		if (rsvps == null) {
+			if (other.rsvps != null)
+				return false;
+		} else if (!rsvps.equals(other.rsvps))
+			return false;
+		if (sentMessages == null) {
+			if (other.sentMessages != null)
+				return false;
+		} else if (!sentMessages.equals(other.sentMessages))
+			return false;
 		if (subscriptions == null) {
 			if (other.subscriptions != null)
 				return false;
@@ -141,9 +204,10 @@ public class BasicUser extends User{
 	@Override
 	public String toString() {
 		return "BasicUser [firstName=" + firstName + ", lastName=" + lastName + ", friendList=" + friendList
-				+ ", subscriptions=" + subscriptions + "]";
+				+ ", subscriptions=" + subscriptions + ", sentMessages=" + sentMessages + ", recievedMessages="
+				+ recievedMessages + ", rsvps=" + rsvps + ", eventMessages=" + eventMessages + "]";
 	}
 	
-
+	
 	
 }
