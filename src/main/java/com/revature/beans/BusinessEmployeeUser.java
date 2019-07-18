37839fdba0734448f6344a6b5082c10364a7de9d
devwithdonald <hend.donald@gmail.com>
2019-07-17 21:48:08 -0400

    completed annotation conversion of business employee

--- src/main/java/com/revature/beans/BusinessEmployeeUser.java
+++ src/main/java/com/revature/beans/BusinessEmployeeUser.java
@@ -3,59 +3,54 @@ package com.revature.beans;
 import javax.persistence.*;
 
 @Entity
-@Table(name="BUSINESS_EMPLOYEE")
+@Table(name="BUSINESS_EMPLOYEE_USER")
 public class BusinessEmployeeUser extends User{
 
-	@Column(name="EMPLOYEE_NAME")
-	private String employerName;
-	
-	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
-	@JoinColumn(name="business_message_id")
-	private BusinessMessage businessMessage;
-	
 	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
 	@JoinColumn(name="business_id")
 	private BusinessUser businessUser;
 	
+	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	@JoinColumn(name="business_message_id")
+	private BusinessMessage businessMessage;
+
 	public BusinessEmployeeUser() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-	
-	public BusinessEmployeeUser(int id, String username, String password) {
-		super(id, username, password);
-	}
-	
-	public BusinessEmployeeUser(String employerName, BusinessMessage businessMessage) {
-		super();
-		this.employerName = employerName;
+
+	public BusinessEmployeeUser(int userId, String username, String password, Role role, BusinessUser businessUser,
+			BusinessMessage businessMessage) {
+		super(userId, username, password, role);
+		this.businessUser = businessUser;
 		this.businessMessage = businessMessage;
 	}
-	
-	public String getEmployerName() {
-		return employerName;
+
+	public BusinessUser getBusinessUser() {
+		return businessUser;
 	}
-	
-	public void setEmployerName(String employerName) {
-		this.employerName = employerName;
+
+	public void setBusinessUser(BusinessUser businessUser) {
+		this.businessUser = businessUser;
 	}
-	
+
 	public BusinessMessage getBusinessMessage() {
 		return businessMessage;
 	}
-	
+
 	public void setBusinessMessage(BusinessMessage businessMessage) {
 		this.businessMessage = businessMessage;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
 		result = prime * result + ((businessMessage == null) ? 0 : businessMessage.hashCode());
-		result = prime * result + ((employerName == null) ? 0 : employerName.hashCode());
+		result = prime * result + ((businessUser == null) ? 0 : businessUser.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -70,19 +65,19 @@ public class BusinessEmployeeUser extends User{
 				return false;
 		} else if (!businessMessage.equals(other.businessMessage))
 			return false;
-		if (employerName == null) {
-			if (other.employerName != null)
+		if (businessUser == null) {
+			if (other.businessUser != null)
 				return false;
-		} else if (!employerName.equals(other.employerName))
+		} else if (!businessUser.equals(other.businessUser))
 			return false;
 		return true;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "BusinessEmployeeUser [employerName=" + employerName + ", businessMessage=" + businessMessage + "]";
+		return "BusinessEmployeeUser [businessUser=" + businessUser + ", businessMessage=" + businessMessage + "]";
 	}
-	
+		
 	
 	
 }
