df176aac3b277f622b335742c3d9198ee6e47d84
Matthew <matt_alston2112@yahoo.com>
2019-07-18 20:34:53 -0400

    angular code

--- HangOutAngularApp/src/app/BasicUser.service.ts
+++ HangOutAngularApp/src/app/BasicUser.service.ts
@@ -1,18 +1,21 @@
 import { Injectable, Inject } from '@angular/core';
 import { BasicUser } from 'src/app/BasicUser';
-import { HttpClient } from '@angular/common/http';
+import { stringify } from 'querystring';
+
 @Injectable({providedIn: 'root'})
 // tslint:disable-next-line: no-unused-expression
 export class BasicUserService {
     constructor() {
     }
 
-    basicUser: BasicUser;
 
     addBasicUser(username: string, password: string, firstName: string, lastName: string) {
         console.log('hello addbasicuser');
         console.log(username, password, firstName, lastName);
-        //return this.http.post();
+        const basic = new BasicUser(firstName, lastName, password, username);
+        console.log(basic);
+        return basic;
 
     }
 }
+
---
+++ HangOutAngularApp/src/app/Role.ts
@@ -0,0 +1,8 @@
+export class Role {
+    roleId: number;
+    roleType: string;
+
+    constructor(roleType: string) {
+        this.roleType = roleType;
+    }
+}
