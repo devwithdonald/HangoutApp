2bca6866516664812c391ccf59bbf71d06bbb294
devwithdonald <hend.donald@gmail.com>
2019-07-20 22:46:48 -0400

    added BasicUserGetEvent Controller

---
+++ src/main/java/com/revature/controllers/BasicUserGetEventsController.java
@@ -0,0 +1,32 @@
+package com.revature.controllers;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.beans.Event;
+import com.revature.services.EventServiceImpl;
+
+@RestController("/BasicUser/PrivateEvents")
+@CrossOrigin(origins = "*")
+public class BasicUserGetEventsController {
+	
+	private static Logger log = Logger.getLogger("DRIVER_LOGGER");
+
+	private EventServiceImpl eventService;
+
+	@Autowired
+	public void setEventService(EventServiceImpl eventService) {
+		this.eventService = eventService;
+	}
+	
+	@GetMapping("/BasicUser/PrivateEvents")
+	public List<Event> basicUserEventGet() {
+		return eventService.getAllBasicUserEvents();
+	}
+
+}
