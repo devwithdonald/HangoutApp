45ea7cac6cedacb2523bcc907d6507bf7aabe66c
Matthew <matt_alston2112@yahoo.com>
2019-07-25 13:25:11 -0400

    adding rsvp after merge

--- src/main/java/com/revature/controllers/BasicUserEventsRSVP.java
+++ src/main/java/com/revature/controllers/BasicUserEventsRSVP.java
@@ -14,16 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.beans.BasicUser;
 import com.revature.beans.Event;
-import com.revature.beans.Role;
 import com.revature.beans.RsvpDTO;
-import com.revature.beans.User;
-import com.revature.dao.EventDao;
-import com.revature.dao.UserDao;
 import com.revature.services.EventService;
 import com.revature.services.RsvpService;
-import com.revature.services.RsvpServiceImpl;
 import com.revature.services.UserService;
 
 @RestController("/BasicUser/Events")
@@ -34,16 +28,9 @@ public class BasicUserEventsRSVP {
 
 	private RsvpService rsvpService;
 	
-	private UserDao userDao;
-	
 	private EventService eventService;
 	
 	private UserService userService;
-	
-	@Autowired
-	public void setUserDao(UserDao userDao) {
-		this.userDao = userDao;
-	}
 	
 	@Autowired
 	public void setuserService(UserService userService) {
@@ -65,15 +52,12 @@ public class BasicUserEventsRSVP {
 		return eventService.getAllPublicEvents();
 	}
 	
-	@PostMapping(path = "/BasicUser/Events", consumes = { "application/json" })
+	@PostMapping(value = "/BasicUser/Events", consumes = { "application/json" })
 	public @ResponseBody Boolean basicUserRSVPPost(@RequestBody RsvpDTO rsvp, HttpSession sess) {
 		System.out.println(rsvp.getEventId());
 		log.log(Level.INFO, "Attempting to RSVP event: " + rsvp.getEventId());
 		
-		//rsvp.setUser(new BasicUser(12, "test_user20", "user20", new Role(3, "BascUser"), "don", "jon"));
 		rsvp.setEvent(eventService.getEventByEventId(rsvp.getEventId()));
-		System.out.println(eventService.getEventByEventId(rsvp.getEventId()));
-		System.out.println(rsvp.getEventId()+rsvp.getStatus()+rsvp.getUser());
 		rsvp.setUser(userService.getBasicUserByUsername(rsvp.getUser1().getUsername()));
 		log.log(Level.INFO, "User id" + rsvp.getUser().getUserId());
 		if(rsvp.getStatus().equals("Accepted"))
@@ -84,7 +68,6 @@ public class BasicUserEventsRSVP {
 			return true;
 		}
 		else if(rsvp.getStatus().equals("Rejected")) {
-		//	rsvp.setEvent(eventService.getEventByEventId(rsvp.getEventId()));
 			rsvpService.rejectRsvp(rsvp);
 			return true;
 		}
